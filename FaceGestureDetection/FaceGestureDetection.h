2cb95ce2370695bdd9601d9029f75ece6f4d80ee
naeem.hussain <naeem.hussain@axabiztech.com>
2022-11-07 15:01:46 +0500

    Initial project setup

---
+++ FaceGestureDetection/FaceGestureDetection.h
@@ -0,0 +1,18 @@
+//
+//  FaceGestureDetection.h
+//  FaceGestureDetection
+//
+//  Created by Naeem Hussain on 07/11/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FaceGestureDetection.
+FOUNDATION_EXPORT double FaceGestureDetectionVersionNumber;
+
+//! Project version string for FaceGestureDetection.
+FOUNDATION_EXPORT const unsigned char FaceGestureDetectionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FaceGestureDetection/PublicHeader.h>
+
+
